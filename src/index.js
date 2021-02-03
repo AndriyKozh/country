979149c2af8f89cc810f3deee7055a6cef26469d
Lucas Neves <lucas54neves@outlook.com>
2021-02-02 23:51:37 -0300

    Add get all countries simple route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import countries from './mock/countries.js'
 
 const app = express()
 
-app.get('/', (request, response) => {
-    return response.json({message:'Hello World!'})
+app.get('/countries', (request, response) => {
+    return response.json(countries)
 })
 
 app.listen(3333, function(){
