6f41f15b9e59f95448a2ea326428f4788af42758
Lucas Neves <lucas54neves@outlook.com>
2021-02-12 16:51:41 -0300

    add countries router

--- src/routes/countries.routes.ts
+++ src/routes/countries.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express'
-import countries from '../mock/countries.json'
+// import countries from '../mock/countries.json'
+
+type CountryType = {
+  name: string;
+  region: string;
+}
+
+const countries: CountryType[] = []
 
 const countriesRoute = Router()
 
-countriesRoute.get('/countries', (request, response) => {
+countriesRoute.get('/', (request, response) => {
   return response.json(countries)
+})
+
+countriesRoute.post('/', (request, response) => {
+
 })
 
 export default countriesRoute
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import usersRoute from './users.routes'
+import countriesRoute from './countries.routes'
 
 const routes = Router()
 
 routes.use('/users', usersRoute)
+routes.use('/countries', countriesRoute)
 
 export default routes
