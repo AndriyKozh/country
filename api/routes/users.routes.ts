a79498e984411f58708d10fe171992435d75decb
Lucas Neves <lucas54neves@outlook.com>
2021-03-04 13:49:23 -0300

    rename /src to /api

--- api/routes/users.routes.ts
+++ api/routes/users.routes.ts
@@ -3,7 +3,7 @@ import createUser from '../services/createUser'
 
 const usersRoute = Router()
 
-usersRoute.post('/new', (request, response) => {
+usersRoute.post('/new', (request: NowRequest, response: NowResponse) => {
   const { name, email, password } = request.body
 
   const user = createUser({
